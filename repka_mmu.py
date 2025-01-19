d43efb6ea5c487fb2f6c375996f48f6c2b34b4ab
Fёdor T <btm.007@mail.ru>
2025-01-19 11:32:29 +0300

    Added pin configuration and mapping

--- repka_mmu.py
+++ repka_mmu.py
@@ -1,10 +1,72 @@
 # Copytright 2025 Fe-Ti
-# Repka Pi 4 Optimal as super simple MMU for custom cpu core
+# A script for Repka Pi 4 Optimal to become simple MMU for custom cpu core
+
+# From CST file:
+# ~ IO_LOC "datainbit" 38;
+# ~ IO_PORT "datainbit" IO_TYPE=LVCMOS33 PULL_MODE=UP;
+# ~ IO_LOC "clk_data" 37;
+# ~ IO_PORT "clk_data" IO_TYPE=LVCMOS33 PULL_MODE=UP;
+# ~ IO_LOC "data_sync_en" 36;
+# ~ IO_PORT "data_sync_en" IO_TYPE=LVCMOS33 PULL_MODE=UP;
+# ~ IO_LOC "clk_sys" 39;
+# ~ IO_PORT "clk_sys" IO_TYPE=LVCMOS33 PULL_MODE=UP;
+# ~ IO_LOC "sys_reset" 25;
+# ~ IO_PORT "sys_reset" IO_TYPE=LVCMOS33 PULL_MODE=UP;
+# ~ IO_LOC "dataoutbit" 26;
+# ~ IO_PORT "dataoutbit" IO_TYPE=LVCMOS33 PULL_MODE=UP DRIVE=8;
+# ~ IO_LOC "memstoref" 27;
+# ~ IO_PORT "memstoref" IO_TYPE=LVCMOS33 PULL_MODE=UP DRIVE=8;
+# ~ IO_LOC "memloadf" 28;
+# ~ IO_PORT "memloadf" IO_TYPE=LVCMOS33 PULL_MODE=UP DRIVE=8;
+#
+# Repka to Tang Nano 9k pin mapping:
+# +--------+------+---------------+
+# |  RPi4o | Tang |  Function     |
+# +--------+------+---------------+
+# |  26 o  | 38 i |  datainbit    |
+# |  24 o  | 37 i |  clk_data     |
+# |  22 o  | 36 i |  data_sync_en |
+# |  40 o  | 39 i |  clk_sys      |
+# |  23 o  | 25 i |  sys_reset    |
+# |  21 i  | 26 o |  dataoutbit   |
+# |  19 i  | 27 o |  memstoref    |
+# |  18 i  | 28 o |  memloadf     |
+# +--------+------+---------------+
 
 import sys
 
-# ~ import OPi.GPIO as GPIO
-# ~ import repka.repka4o
+import OPi.GPIO as GPIO
+import repka.repka4o
+
+# Setup GPIO
+GPIO.setmode(repka.repka4o.BOARD)
+
+# input
+PIN_DATAOUTBIT  = 21
+PIN_MEMSTOREF   = 19
+PIN_MEMLOADF    = 18
+INPUT_PINS = [
+    PIN_DATAOUTBIT,
+    PIN_MEMSTOREF,
+    PIN_MEMLOADF
+    ]
+GPIO.setup(INPUT_PINS, GPIO.IN)
+
+# output
+PIN_DATAINBIT   = 26
+PIN_CLK_DATA    = 24
+PIN_DATA_SYNC_EN= 22
+PIN_CLK_SYS     = 40
+PIN_SYS_RESET   = 23
+OUTPUT_PINS = [
+    PIN_DATAINBIT,
+    PIN_CLK_DATA,
+    PIN_DATA_SYNC_EN,
+    PIN_CLK_SYS,
+    PIN_SYS_RESET
+    ]
+GPIO.setup(OUTPUT_PINS, GPIO.OUT)
+
 
 if len(sys.argv) < 2:
     print("No program file provided.\nShould be verilog vectors as in.")
@@ -16,6 +78,7 @@ if len(sys.argv) > 2:
 memory = dict()
 program = dict()
 
+# Read program from file
 address = 0
 with open(sys.argv[1]) as ifile:
     for line in ifile:
@@ -26,9 +89,11 @@ with open(sys.argv[1]) as ifile:
                 program[address//4*4 + (3-address%4)] = int(lex,16)
                 address += 1
 
+# Print program to console
 for i in range(7):
-    print(f"{hex(i)[2:].zfill(8)}:", end=' ') 
+    print(f"{hex(i)[2:].zfill(8)}:", end=' ')
     for j in range(4):
         print(f"{hex(program[i*4+j])[2:].zfill(2)}", end=' ')
     print()
+
 
